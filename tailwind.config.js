5900723eec1ac20945b3b8bb2556869585ac5701
eliezerfan123 <eliezerfan123@gmail.com>
2025-01-20 15:06:00 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{html,js,astro}'],
+      theme: {
+          extend: {
+
+            backgroundColor: {
+
+"azul-fondo": "#1F1F27",
+"verde-boton": "#10C280"
+
+            }
+          },
+            },
+              plugins: [],
+              };
